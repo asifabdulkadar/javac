91cc887937579bc9ba8fe35fb98f77972c87054f
asifabdulkadar <125760404+asifabdulkadar@users.noreply.github.com>
2025-02-10 21:57:23 +0530

    Update PerfectSquare.java

--- PerfectSquare.java
+++ PerfectSquare.java
@@ -5,7 +5,7 @@ public class PerfectSquare {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int num = scanner.nextInt();
-        scanner.close();
+        
 
         int sqrt = (int) Math.sqrt(num);
         if (sqrt * sqrt == num) {
