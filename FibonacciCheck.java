c4e13559b46b6d36a0e2ff3de6b98b8e0c4a579d
asifabdulkadar <125760404+asifabdulkadar@users.noreply.github.com>
2025-02-10 21:30:54 +0530

    Update FibonacciCheck.java

--- FibonacciCheck.java
+++ FibonacciCheck.java
@@ -3,23 +3,28 @@ import java.util.Scanner;
 public class FibonacciCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt(), a = 0, b = 1, c = 0;
-
-        if (n == 0 || n == 1) {
-            System.out.println(n + " is a Fibonacci number.");
-        } else {
-            while (c < n) {
-                c = a + b;
-                a = b;
-                b = c;
-            }
-            if (c == n) {
-                System.out.println(n + " is a Fibonacci number.");
-            } else {
-                System.out.println(n + " is not a Fibonacci number.");
-            }
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        if(n==0||n==1){
+            System.out.println(0);
         }
-        sc.close();
+        else{
+            for(int i=1;i<=n;i++){
+                c=a+b;
+                a=b;
+                b=c;
+                System.out.println(c);
+            }
+
+        }
+        if(n==c){
+            System.out.println("its fibonacci number");
+        }
+        else{
+            System.out.println("its not fibonacci number");
+        }
+
     }
 }
