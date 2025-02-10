7654a204531d2d4034ffa499b4834a534663cba9
asifabdulkadar <125760404+asifabdulkadar@users.noreply.github.com>
2025-02-10 21:56:48 +0530

    Add files via upload

---
+++ PerfectSquare.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PerfectSquare {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        int sqrt = (int) Math.sqrt(num);
+        if (sqrt * sqrt == num) {
+            System.out.println(num + " is a perfect square.");
+        } else {
+            System.out.println(num + " is not a perfect square.");
+        }
+    }
+}
