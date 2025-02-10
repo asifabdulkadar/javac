321fd58fa2f0f5a7e87dd608e9b0b68f93a1152d
asifabdulkadar <125760404+asifabdulkadar@users.noreply.github.com>
2025-02-10 21:22:43 +0530

    Update ArmstrongNumber.java

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,34 +1,25 @@
-import java.util.Scanner;
-
-public class ArmstrongNumber {
+public class armstrongNumber {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt(), original = n, sum = 0, count = 0;
+        int a=246700;
+        int rem=0;
+        int sum=0;
+        int temp=a;
 
-        int temp = n;
-        while (temp > 0) {
-            count++;
-            temp /= 10;
+        while(a>0){
+            rem=a%10;
+            int cube=rem;
+            cube=cube*cube*cube;
+            int digits= String.valueOf(a).length();
+            sum=sum+cube;
+            a=a/10;
+            sum+=(int) Math.pow(digits,digits);
+            a/=10;
         }
-
-        temp = n;
-        while (temp > 0) {
-            int digit = temp % 10;
-            int power = 1, i = 0;
-            while (i < count) {
-                power *= digit;
-                i++;
-            }
-            sum += power;
-            temp /= 10;
+        if (sum==temp){
+            System.out.println("Armstrong Number");
         }
-
-        if (sum == original) {
-            System.out.println(original + " is an Armstrong number.");
-        } else {
-            System.out.println(original + " is not an Armstrong number.");
+        else{
+            System.out.println("Not Armstrong Number");
         }
-        sc.close();
     }
 }
