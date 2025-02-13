6c87da52af11bbbacafba9635bf3ac372edb3da5
asifabdulkadar <125760404+asifabdulkadar@users.noreply.github.com>
2025-02-13 09:16:25 +0530

    Update posOrNeg.java

--- posOrNeg.java
+++ posOrNeg.java
@@ -4,7 +4,7 @@ public class posOrNeg
 	public static void main(String[] args) {
 	    Scanner sc=new Scanner(System.in);
 	    int n=sc.nextInt();
-	    if(n%2==0){
+	    if(n>0){
 	        System.out.print("it's positive integer");
 	    }
 	    else{
